dd1a83bbb3b9fbd91624f9ea908f5b89191f3a1f
Alyssa Crechiolo <alyssacrechiolo@gmail.com>
2020-07-17 13:02:00 -0400

    added resume component, updated aboutme component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
-
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
-  { path: "mainpage", component: MainPageComponent },
-  { path: "", redirectTo: "/mainpage", pathMatch: "full" },
-  { path: "**", redirectTo: "/mainpage", pathMatch: "full" }
-
+  { path: 'mainpage', component: MainPageComponent },
+  { path: 'aboutme', component: AboutMeComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: '', redirectTo: '/mainpage', pathMatch: 'full' },
+  { path: '**', redirectTo: '/mainpage', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutMeComponent,
+    ResumeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
